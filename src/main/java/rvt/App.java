b3b667808763c8df155a94eb81475fba8d4450cd
Vadims Malickis <117085106+VadimsMalickis@users.noreply.github.com>
2025-11-04 11:54:24 +0000

    first full implementation

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,14 +1,7 @@
 package rvt;
 
 public class App {
-    public static void main(String[] args) {
-        SimpleHashTable table = new SimpleHashTable(1000);
-
-        for (int i = 1; i <= table.size(); i++) {
-            table.insert(i, "Student Nr. " + i);
-        }
-        System.out.println(table.getProbCount());
-    }   
+   
     public static int hash(int k) {
         return k % (7 + 3);
     }
--- src/main/java/rvt/SimpleHashTable.java
+++ src/main/java/rvt/SimpleHashTable.java
@@ -1,11 +1,19 @@
 package rvt;
 
-
 public class SimpleHashTable {
+    public static void main(String[] args) {
+        SimpleHashTable table = new SimpleHashTable(1000);
+        for (int i = 1; i <= table.getCapacity(); i++) {
+            table.insert(i, "Student Nr. " + i);
+        }
+        System.out.println(table.probCount);
+    }
 
     private final int capacity;
     private int elementCount;
     private String[] data;
+
+    // For hash table filling testing purposes
     private int probCount;
 
     public SimpleHashTable(int c) {
@@ -14,28 +22,25 @@ public class SimpleHashTable {
         data = new String[c];
     }
 
-    public int size() {
+    public int getCapacity() {
         return this.capacity;
-    }
-    public int getProbCount() {
-        return this.probCount;
     }
 
     public void insert(int key, String value) {
         if (this.elementCount == this.capacity) {
-            throw new IllegalStateException("Hash table is full (max "  + this.capacity + " elements)");
+            throw new IllegalStateException("Hash table is full (max " + this.capacity + " elements)");
         }
-        int probingAttempt = 0;
-        int hashCode = (hashFunction(key) + probingAttempt) % this.capacity;
+        int hashCode = hashFunction(key);
         boolean isSpotFree = data[hashCode] == null;
         if (isSpotFree) {
             data[hashCode] = value;
             elementCount++;
         } else {
+            int probingAttempt = 0;
             while (!isSpotFree) {
-                this.probCount++;
+                this.probCount++; // testing
                 probingAttempt++;
-                hashCode = (hashFunction(key) + probingAttempt) % this.capacity;
+                hashCode = hashFunction(key) + probingAttempt;
                 isSpotFree = data[hashCode] == null;
                 if (isSpotFree) {
                     data[hashCode] = value;
@@ -48,11 +53,51 @@ public class SimpleHashTable {
     }
 
     public void remove(int key) {
-        data[hashFunction(key)] = null;
+        if (this.elementCount == 0) {
+            throw new IllegalStateException("Hash table is empty.");
+        }
+        int hashCode = hashFunction(key);
+        boolean isElementFound = data[hashCode] != null;
+        if (isElementFound) {
+            data[hashCode] = null;
+            elementCount--;
+        } else {
+            int probingAttempt = 0;
+            while (!isElementFound) {
+                this.probCount++;
+                probingAttempt++;
+                hashCode = hashFunction(key) + probingAttempt;
+                isElementFound = data[hashCode] != null;
+                if (isElementFound) {
+                    data[hashCode] = null;
+                    elementCount--;
+                    break;
+                }
+            }
+        }
     }
 
     public String find(int key) {
-        return data[hashFunction(key)];
+        if (this.elementCount == 0) {
+            throw new IllegalStateException("Hash table is empty.");
+        }
+        int hashCode = hashFunction(key);
+        boolean isElementFound = data[hashCode] != null;
+        if (isElementFound) {
+            return data[hashCode];
+        } else {
+            int probingAttempt = 0;
+            while (!isElementFound) {
+                this.probCount++;
+                probingAttempt++;
+                hashCode = hashFunction(key) + probingAttempt;
+                isElementFound = data[hashCode] != null;
+                if (isElementFound) {
+                    return data[hashCode];
+                }
+            }
+        }
+        return null;
     }
 
     private int hashFunction(int key) {
